c919cbca8faeb47dffd3fae22c9f0e87008ed8dd
caoxin <caoxin1988s@gmail.com>
2018-11-28 17:17:41 +0800

    update run.sh
    Signed-off-by: caoxin <caoxin1988s@gmail.com>

--- run.sh
+++ run.sh
@@ -1,2 +1,22 @@
 git pull
-/opt/anaconda3/bin/python fast_app.py --write_mongo=True --write_mysql=True
+
+cur_date="log""`date +%Y%m%d-%H%M`"
+file_name=$cur_date
+echo $file_name
+
+if [ ! -d "log" ]; then
+    mkdir "log"
+fi
+
+cd log
+
+if [ -f $file_name ]; then
+    echo "is exist"
+else
+    echo "not exist"
+    touch $file_name
+fi
+
+cd ..
+
+/opt/anaconda3/bin/python fast_app.py --write_mongo=True --write_mysql=True >"log/"$file_name
