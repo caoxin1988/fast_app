ae3f4860fa480214b13a6f98914330c48ab82276
caoxin <caoxin1988s@gmail.com>
2018-12-05 13:27:18 +0800

    delete some old codes
    Signed-off-by: caoxin <caoxin1988s@gmail.com>

--- algo_lru.py
+++ algo_lru.py
@@ -26,112 +26,6 @@ def delete_all_data(collec : str):
 def close_mongodb():
     mongodb.close()
 
-def write_result_file(df : pd.DataFrame, date : str, update_result : bool):
-    result_file = common.RESULT_DIR + date + common.RESULT_CSV_SUFFIX
-
-    if not update_result:
-        return
-
-    start = datetime.datetime.now()
-
-    result = []
-    for mac_addr in df.index.levels[0]:
-        df_sub = df.loc[mac_addr]
-        if len(df_sub) > 2:
-            df_sub = df_sub.sort_values(by = 'app_cnt', ascending = False)[:common.TARGET_NUM]
-
-        for app_name in df_sub.index:
-            res_sub = []
-            res_sub.append(mac_addr)
-            res_sub.append(app_name)
-
-            result.append(res_sub)
-
-    with open(result_file, 'w', newline = '') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerows(result)
-
-    print('#### Runtime = ', datetime.datetime.now() - start)
-
-'''algothrims for lru '''
-def calculate_target_name_with_LRU(date_p : str, date_t : str, write_mongo : bool = False,
-    update_result : bool = True):
-
-    result_file = common.RESULT_DIR + date_p + common.RESULT_CSV_SUFFIX
-    app_df = None
-    if os.path.exists(result_file):
-        apps = AppResult(result_file)
-        app_df = apps.get_dataframe()
-    else:
-        # data from former day
-        apps = Apps(date_p)
-
-        app_df = apps.get_dataframe()
-        app_df = app_df[['mac', 'app_name']].groupby('mac').agg({'app_name':'value_counts'}).rename(columns={'app_name':'app_cnt'})
-        # generate result for prediction
-        write_result_file(app_df, date_p, update_result)
-
-        apps = AppResult(result_file)
-        app_df = apps.get_dataframe()
-
-    # merge the data from former day
-#     date_f = datetime.date(int(date_p[:4]), int(date_p[4:6]), int(date_p[6:])) - datetime.timedelta(days = 1)
-#     apps = Apps(CSV_DIR + date_f.strftime('%Y%m%d') + APP_CSV_SUFFIX)
-#     app_df_f = remove_duplicates_of_app(apps)
-#     app_df_f = app_df_f[['mac', 'app_name']]
-#     print(app_df_f.shape)
-
-#     app_df = pd.concat([app_df, app_df_f], ignore_index = True)
-#     print(app_df.shape)
-
-#     app_df.drop_duplicates(inplace = True)
-#     print(app_df.shape)
-
-    # data for target day
-    apps = Apps(date_t)
-    app_df1 = apps.get_dataframe()
-    app_df1 = app_df1[['mac', 'app_name']].groupby('mac').agg({'app_name':'value_counts'}).rename(columns={'app_name':'app_cnt'})
-    # generate result for prediction
-    write_result_file(app_df1, date_t, update_result)
-
-    app_df1 = app_df1.reset_index()
-
-    # merge two dataframe for the result
-    df = pd.merge(app_df, app_df1, on = ['mac', 'app_name'])
-
-    # get app and user datas
-    app_start_times = int(app_df1.app_cnt.sum())
-    user_count_apps = int(app_df1.mac.drop_duplicates().count())
-    app_counts = int(app_df1.app_name.drop_duplicates().count())
-
-    app_start_info_dict = {
-            'date' : datetime.date(int(date_t[:4]), int(date_t[4:6]), int(date_t[6:])).strftime('%Y%m%d %a'),
-            'app_start_times' : app_start_times,
-            'user_count_apps' : user_count_apps,
-            'app_counts' : app_counts }
-    # app_start_info_dict = {'date': '20181118 Sun', 'app_start_times': 434575, 'user_count_apps': 127219, 'app_counts': 1999}
-    print(app_start_info_dict)
-
-    if write_mongo:
-        print('#### start writing app_start_info_dict into mongoDB')
-        mongodb.insert_element_to_collection('apps_info', app_start_info_dict)
-
-    # get the target result
-    app_open_times_ratio = round((df.app_cnt.sum() / app_df1.app_cnt.sum()), 2)
-    target_user_ratio = round((df.mac.drop_duplicates().count() / app_df.mac.count()), 2)
-    target_user_app_ratio = round((df.mac.count() / app_df.mac.count()), 2)
-
-    result_dict = {
-            'date' : datetime.date(int(date_t[:4]), int(date_t[4:6]), int(date_t[6:])).strftime('%Y%m%d %a'), 
-            'app_open_times_ratio' : app_open_times_ratio,
-            'target_user_ratio' : target_user_ratio,
-            'target_user_app_ratio' : target_user_app_ratio }
-    print(result_dict)
-
-    if write_mongo:
-        print('#### start writing result_dict into mongoDB')
-        mongodb.insert_element_to_collection('predict_ratio', result_dict)
-
 def merge_dataframe_with_days(df : pd.DataFrame, date : str, day_num : int = 0):
     app_df = df
 
@@ -290,6 +184,5 @@ def update_mysql(date : str):
 
 if __name__ == '__main__':
     init_mongodb('localhost')
-    # calculate_target_name_with_LRU('20181117', '20181118')
     calculate_target_name_with_LRU_with_days('20181126', '20181127', update_result=True, day_num =1)
     close_mongodb()
