f676b9d2662ad73dbb769527fbd76175cf0c41c8
caoxin <caoxin1988s@gmail.com>
2018-12-27 11:22:31 +0800

    update common.py
    Signed-off-by: caoxin <caoxin1988s@gmail.com>

--- common.py
+++ common.py
@@ -9,10 +9,10 @@ RESULT_DIR = './result/'
 RESULT_CSV_SUFFIX = '-result.csv'
 TARGET_NUM = 2
 
-TARGET_RESULT_DIR = '/data/fast_app/'
-TARGET_RESULT_SPLIT_DIR = '/data/fast_app/split/'
-TARGET_RESULT_FILE = '/data/fast_app/target_result'
-TARGET_RESULT_SPLIT_FILE = '/data/fast_app/split/target_result-'
+TARGET_RESULT_DIR = './fast_app_result/'
+TARGET_RESULT_SPLIT_DIR = TARGET_RESULT_DIR + 'split/'
+TARGET_RESULT_FILE = TARGET_RESULT_DIR + 'target_result'
+TARGET_RESULT_SPLIT_FILE = TARGET_RESULT_SPLIT_DIR + 'target_result-'
 TARGET_RESULT_FILE_SUFFIX = '.txt'
 
 TARGET_FILE_MAX_SIZE = 1000 * 1024
@@ -22,7 +22,7 @@ POST_URL = 'http://172.17.12.130:8580/master-admin/api/v1/rom-data'
 GET_URL = 'http://172.17.12.130:8580/master-admin/api/v1/clear/rom-data'
 AUTH_SUFFIX = 'file-setting-log-upload'
 
-HIVE_DIR = '/data/hive_run_data/'
+HIVE_DIR = './hive_run_data/'
 
 SQL_SERVER = '172.17.7.26'
 SQL_DB = 'rom_charts'
